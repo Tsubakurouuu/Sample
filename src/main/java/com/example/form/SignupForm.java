fc1202ac47e80fc625d96d64a484ba73a5acaebe
Tsubakurouuu <yzpolice2896@icloud.com>
2023-02-03 10:49:15 +0900

    Spring Bootが丸わかりSpring解体新書第2版 6章 6.3.1 途中

--- src/main/java/com/example/form/SignupForm.java
+++ src/main/java/com/example/form/SignupForm.java
@@ -1,10 +1,16 @@
 package com.example.form;
 
 import java.util.Date;
-
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.Data;
+
 
 @Data
 public class SignupForm {
